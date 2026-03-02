c6e71ee125fa622ec14727fc89a790a7a1a21370
Krzysztof_Serkowski <krzysztof_serkowski@epam.com>
2026-03-02 12:37:27 +0100

    Java Prompt #1

---
+++ src/test/java/com/serkowski/SpringAi2SubstackHeroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.serkowski;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAi2SubstackHeroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
