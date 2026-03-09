165fc1a8ec9c8761f18658fce646f904af3413c2
Krzysztof_Serkowski <krzysztof_serkowski@epam.com>
2026-03-09 11:37:41 +0100

    Movie recommendation

--- src/main/java/com/serkowski/controller/ChatController.java
+++ src/main/java/com/serkowski/controller/ChatController.java
@@ -1,11 +1,14 @@
 package com.serkowski.controller;
 
+import com.serkowski.model.MovieRecommendation;
 import com.serkowski.service.ChatService;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
+
+import java.util.List;
 
 @RestController
 public class ChatController {
@@ -24,6 +27,21 @@ public class ChatController {
     @PostMapping(value = "/chatStream", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
     public Flux<String> chatStream(@RequestBody TextRequest request) {
         return chatService.chatStream(request.message);
+    }
+
+    @PostMapping("/movieRecommendation")
+    public MovieRecommendation movieRecommendation(@RequestBody TextRequest request) {
+        return chatService.movieRecommendation(request.message);
+    }
+
+    @PostMapping("/movieRecommendations")
+    public List<MovieRecommendation> movieRecommendations(@RequestBody TextRequest request) {
+        return chatService.movieRecommendations(request.message);
+    }
+
+    @PostMapping("/movieRecommendationSchema")
+    public MovieRecommendation movieRecommendationSchema(@RequestBody TextRequest request) {
+        return chatService.movieRecommendationViaOutputSchema(request.message);
     }
 
 
--- src/main/java/com/serkowski/service/ChatService.java
+++ src/main/java/com/serkowski/service/ChatService.java
@@ -1,8 +1,14 @@
 package com.serkowski.service;
 
+import com.serkowski.model.MovieRecommendation;
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.converter.BeanOutputConverter;
+import org.springframework.ai.ollama.api.OllamaChatOptions;
+import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
+
+import java.util.List;
 
 @Service
 public class ChatService {
@@ -25,5 +31,33 @@ public class ChatService {
                 .user(message)
                 .stream()
                 .content();
+    }
+
+    public MovieRecommendation movieRecommendation(String message) {
+        return chatClient.prompt()
+                .user(message)
+                .call()
+                .entity(MovieRecommendation.class);
+    }
+
+    public List<MovieRecommendation> movieRecommendations(String message) {
+        return chatClient.prompt()
+                .user(message)
+                .call()
+                .entity(new ParameterizedTypeReference<List<MovieRecommendation>>() {
+                });
+    }
+
+    public MovieRecommendation movieRecommendationViaOutputSchema(String message) {
+        var converter = new BeanOutputConverter<>(MovieRecommendation.class);
+
+        String response = chatClient.prompt()
+                .options(OllamaChatOptions.builder()
+                        .outputSchema(converter.getJsonSchema())
+                        .build())
+                .user(message)
+                .call()
+                .content();
+        return converter.convert(response);
     }
 }
