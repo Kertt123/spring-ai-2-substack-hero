3e815a45d4e5d1784434ad6884e26f545307ec8f
Krzysztof_Serkowski <krzysztof_serkowski@epam.com>
2026-04-23 12:22:02 +0200

    Prompt 7

--- src/main/java/com/serkowski/configuration/Configuration.java
+++ src/main/java/com/serkowski/configuration/Configuration.java
@@ -7,6 +7,11 @@ import org.springframework.ai.chat.memory.ChatMemory;
 import org.springframework.ai.chat.memory.MessageWindowChatMemory;
 import org.springframework.ai.chat.memory.repository.jdbc.JdbcChatMemoryRepository;
 import org.springframework.ai.chat.model.ChatModel;
+import org.springframework.ai.rag.advisor.RetrievalAugmentationAdvisor;
+import org.springframework.ai.rag.generation.augmentation.ContextualQueryAugmenter;
+import org.springframework.ai.rag.preretrieval.query.expansion.MultiQueryExpander;
+import org.springframework.ai.rag.preretrieval.query.transformation.RewriteQueryTransformer;
+import org.springframework.ai.rag.retrieval.search.VectorStoreDocumentRetriever;
 import org.springframework.ai.vectorstore.VectorStore;
 import org.springframework.context.annotation.Bean;
 
@@ -22,10 +27,34 @@ public class Configuration {
     }
 
     @Bean
-    public ChatClient chatClient(ChatModel chatModel, ChatMemory chatMemory) {
+    public ChatClient chatClient(ChatModel chatModel, ChatMemory chatMemory, VectorStore vectorStore) {
         return ChatClient.builder(chatModel)
                 .defaultAdvisors(MessageChatMemoryAdvisor.builder(chatMemory)
-                        .build()
+                                .build(),
+//                        QuestionAnswerAdvisor.builder(vectorStore)
+//                                .searchRequest(SearchRequest.builder()
+//                                        .topK(4)
+//                                        .similarityThreshold(0.3)
+//                                        .build())
+//                                .build()
+                        RetrievalAugmentationAdvisor.builder()
+                                .queryTransformers(RewriteQueryTransformer.builder()
+                                        .chatClientBuilder(ChatClient.builder(chatModel))
+                                        .build())
+                                .queryExpander(MultiQueryExpander.builder()
+                                        .chatClientBuilder(ChatClient.builder(chatModel))
+                                        .numberOfQueries(3)
+                                        .includeOriginal(true)
+                                        .build())
+                                .queryAugmenter(ContextualQueryAugmenter.builder()
+                                        .allowEmptyContext(true)
+                                        .build())
+                                .documentRetriever(VectorStoreDocumentRetriever.builder()
+                                        .vectorStore(vectorStore)
+                                        .similarityThreshold(0.5)
+                                        .topK(5)
+                                        .build())
+                                .build()
                 )
                 .build();
     }
--- src/main/java/com/serkowski/service/ChatService.java
+++ src/main/java/com/serkowski/service/ChatService.java
@@ -1,6 +1,7 @@
 package com.serkowski.service;
 
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.advisor.vectorstore.QuestionAnswerAdvisor;
 import org.springframework.ai.chat.memory.ChatMemory;
 import org.springframework.stereotype.Service;
 
@@ -17,6 +18,9 @@ public class ChatService {
         return chatClient.prompt()
                 .user(message)
                 .advisors(a -> a.param(ChatMemory.CONVERSATION_ID, conversationId))
+                .advisors(a -> a.param(
+                        QuestionAnswerAdvisor.FILTER_EXPRESSION,
+                        "filename == 'dysonManual.pdf'"))
                 .call()
                 .content();
     }
